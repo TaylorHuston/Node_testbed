c99916f05db6005106922152812d4de1884dcbbc
Taylor Huston <taylor.a.huston@gmail.com>
2015-02-16 18:35:09 -0700

    Add timeout.js

---
+++ timeouts.js
@@ -0,0 +1,17 @@
+//Basic snippet to demonstrate some basic timeouts in Node
+function simpleTimeout(consoleTimer) {
+    conole.timeEnd(consoleTimer);
+}
+
+console.time("twoSecond");
+setTimeout(simpleTimeout, 2000, "twoSecond");
+
+console.time("oneSecond");
+setTimeout(simpleTimeout, 1000, "oneSecond");
+
+console.time("fiveSecond");
+setTimeout(simpleTimeout, 5000, "fiveSecond");
+           
+console.time("50MilliSecond");
+setTimeout(simpleTimeout, 50, "50MilliSecond");
+
